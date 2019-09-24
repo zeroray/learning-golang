3420f901099730916914c185a764af34cfe5fc29
Daniel Fuentes Cea <dfuentesc@falabella.cl>
2019-09-24 15:55:22 -0300

    basic types

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
+	"math/cmplx"
+)
 
-	"github.com/zeroray/learning-golang/ifflow"
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
 func main() {
-	fmt.Println(
-		ifflow.Pow(3, 2, 10),
-		ifflow.Pow(3, 3, 20),
-	)
+	const f = "%T(%v)\n"
+	fmt.Printf(f, ToBe, ToBe)
+	fmt.Printf(f, MaxInt, MaxInt)
+	fmt.Printf(f, z, z)
 }
