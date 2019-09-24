73558f380dbabfee09537834b57eb7c9f9e38ca8
Daniel Fuentes Cea <dfuentesc@falabella.cl>
2019-09-24 11:36:09 -0300

    add practice folder and basics examples

---
+++ practice/practice.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// func main() {
+// 	fmt.Println("Happy", math.Pi, "Day")
+// }
+
+// func main() {
+// 	fmt.Printf("Now you have %g problems.", math.Nextafter(2, 3))
+// }
+
+func main() {
+	fmt.Println(math.Pi)
+}
