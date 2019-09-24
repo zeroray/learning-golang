dd109d8dbcf824e7f9051d688739cd52d9fbe1e7
Daniel Fuentes Cea <dfuentesc@falabella.cl>
2019-09-24 12:17:12 -0300

    constants

---
+++ practice/constants.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// const Pi = 3.14
+
+// func main() {
+// 	const World = "世界"
+// 	fmt.Println("Hello", World)
+// 	fmt.Println("Happy", Pi, "Day")
+
+// 	const Truth = true
+// 	fmt.Println("Go rules?", Truth)
+// }
+
+const (
+	Big   = 1 << 100
+	Small = Big << 99
+)
+
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
+
+func main() {
+	//fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
+	//fmt.Println(needInt(Big))
+}
