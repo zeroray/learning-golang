ca90f45e43f32db207b30ddfc6d58820c234aed1
Daniel Fuentes Cea <dfuentesc@falabella.cl>
2019-09-24 12:07:27 -0300

    variables

---
+++ practice/variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// var x, y, z int
+// var c, python, java bool
+
+// var x, y, z int = 1, 2, 3
+// var c, python, java = true, false, "No!"
+
+func main() {
+	// short declare
+	var x, y, z int = 1, 2, 3
+	c, python, java := true, false, "no!"
+	fmt.Println(x, y, z, c, python, java)
+}
