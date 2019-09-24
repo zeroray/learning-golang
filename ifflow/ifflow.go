fc07319c3cb559e05563afc75cbb35d9c2df2557
Daniel Fuentes Cea <dfuentesc@falabella.cl>
2019-09-24 15:47:38 -0300

    if else

--- ifflow/ifflow.go
+++ ifflow/ifflow.go
@@ -15,6 +15,9 @@ func Sqrt(x float64) string {
 func Pow(x, n, lim float64) float64 {
 	if v := math.Pow(x, n); v < lim {
 		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
 	}
+	// can't use v here, though
 	return lim
 }
