21d31f8ffd1509ac5d6f624eab4ee2d5ed8cf9ff
Daniel Fuentes Cea <dfuentesc@falabella.cl>
2019-09-25 11:00:26 -0300

    maps alter

--- exampleStruct/exampleStruct.go
+++ exampleStruct/exampleStruct.go
@@ -2,30 +2,19 @@ package exampleStruct
 
 import "fmt"
 
-type Vertex struct {
-	Lat, Long float64
-}
-
-// var m map[string]Vertex
-// var m = map[string]Vertex{
-// 	"My Company": Vertex{
-// 		33.333333, -77.7777777,
-// 	},
-// 	"Other Company": Vertex{
-// 		44.444444, -55.000005,
-// 	},
-// }
-
-var m = map[string]Vertex{
-	"My Company":    {33.333333, -77.7777777},
-	"Other Company": {44.444444, -55.000005},
-}
-
 func DeclareVertex() {
-	// m = make(map[string]Vertex)
-	// m["My Company"] = Vertex{
-	// 	33.33333332, -77.777777,
-	// }
-	// fmt.Println(m["My Company"])
-	fmt.Println(m)
+	m := make(map[string]int)
+
+	m["Response"] = 42
+	fmt.Println("The value:", m["Response"])
+
+	m["Response"] = 48
+	fmt.Println("The value:", m["Response"])
+
+	delete(m, "Response")
+	fmt.Println("The value:", m["Response"])
+
+	v, ok := m["Response"]
+	fmt.Println("The value:", v, "Present?", ok)
+
 }
