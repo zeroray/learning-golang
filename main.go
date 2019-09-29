89cee6e79986851d0486bcfd900ed452dbf93bf6
Daniel Fuentes Cea <dfuentesc@falabella.cl>
2019-09-29 18:17:48 -0300

    methods with my own type

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/zeroray/learning-golang/methods"
+	"math"
 )
 
-
-
 func main() {
-	v := methods.Vertex{3,4}
+/*	v := methods.Vertex{3,4}
 	fmt.Println(v.Abs())
+*/
+
+	f := methods.MyFloat(-math.Sqrt(2))
+	fmt.Println(f.Abs())
 }
--- methods/methods.go
+++ methods/methods.go
@@ -15,3 +15,12 @@ func (v Vertex) Abs() float64 {
 func Abs(v Vertex) float64 {
 	return math.Sqrt(v.X * v.X + v.Y * v.Y)
 }
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
