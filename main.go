5be35bc60f87dcb01c05356c816441d45b7421d5
Daniel Fuentes Cea <dfuentesc@falabella.cl>
2019-09-29 18:01:10 -0300

    methods with receiver

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/zeroray/learning-golang/functions"
+	"github.com/zeroray/learning-golang/methods"
 )
 
-// https://en.wikipedia.org/wiki/Fibonacci_number
+
 
 func main() {
-	f := functions.Fibonacci()
-	for i := 0; i < 20; i++ {
-		fmt.Println(f(i))
-	}
+	v := methods.Vertex{3,4}
+	fmt.Println(v.Abs())
 }
