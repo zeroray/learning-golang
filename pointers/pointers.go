50dbad31bedf855ea160b92e2517c7986e8240a7
Daniel Fuentes Cea <dfuentesc@falabella.cl>
2019-09-29 21:20:03 -0300

    pointers o not pointers with struct

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -9,6 +9,11 @@ type Vertex struct{
 	X, Y float64
 }
 
+type User struct {
+	name string
+	age int
+}
+
 func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
@@ -18,8 +23,22 @@ func (v Vertex) Scale(f float64) {
 	v.Y = v.Y * f
 }
 
+// Change u User to u *User and vice versa
+func (u *User) changeName(name string) {
+	u.name = name
+}
+
+// Change u User to u *User and vice versa
+func (u *User) birtday() {
+	u.age = u.age + 1
+}
+
 func Main() {
-	v := Vertex{3,4}
-	v.Scale(10)
-	fmt.Println(v.Abs())
+	u := User{"Daniel", 32}
+	fmt.Println(u)
+	u.birtday()
+	fmt.Println(u)
+	u.changeName("Daniel Fuentes")
+	fmt.Println(u)
+
 }
