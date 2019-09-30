975cee738ab5e7bb9561360efb5000ecab197395
Daniel Fuentes Cea <dfuentesc@falabella.cl>
2019-09-29 22:52:10 -0300

    methods and pointer indirection 2

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -2,39 +2,28 @@ package pointers
 
 import (
 	"fmt"
+	"math"
 )
 
 type Vertex struct{
 	X, Y float64
 }
 
-/*func Abs(v Vertex) float64 {
+func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-func Scale(v *Vertex, f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
-}*/
-
-func (v *Vertex) Scale(f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
-}
-
-func ScaleFunc(v *Vertex, f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
+func AbsFunc(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 func Main() {
 	v := Vertex{3,4}
-	v.Scale(2)
-	ScaleFunc(&v, 10)
+	fmt.Println(v.Abs())
+	fmt.Println(AbsFunc(v))
 
-	p := &Vertex{4,3}
-	p.Scale(3)
-	ScaleFunc(p, 8)
+	p := &Vertex{3,4}
+	fmt.Println(p.Abs())
+	fmt.Println(AbsFunc(*p))
 
-	fmt.Println(v, p)
 }
