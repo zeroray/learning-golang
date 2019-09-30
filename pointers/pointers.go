c5d2fa1761e27086ee4c6a3368bc8c6a218e0aff
Daniel Fuentes Cea <dfuentesc@falabella.cl>
2019-09-29 23:42:43 -0300

    choosing a value or pointer receiver

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -9,21 +9,19 @@ type Vertex struct{
 	X, Y float64
 }
 
-func (v Vertex) Abs() float64 {
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func (v *Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-func AbsFunc(v Vertex) float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
-}
 
 func Main() {
-	v := Vertex{3,4}
-	fmt.Println(v.Abs())
-	fmt.Println(AbsFunc(v))
-
-	p := &Vertex{3,4}
-	fmt.Println(p.Abs())
-	fmt.Println(AbsFunc(*p))
-
+	v := &Vertex{3,4}
+	fmt.Printf("Before scaling: %v, Abs: %v\n", v, v.Abs())
+	v.Scale(5)
+	fmt.Printf("After scaling: %v, Abs: %v\n", v, v.Abs())
 }
